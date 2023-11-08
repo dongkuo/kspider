20d3636270ceee34de3384fd84c8e0c48555544a
derker <16153284+dongkuo@users.noreply.github.com>
2023-11-08 17:59:50 +0800

    init

--- src/main/kotlin/site/derker/kspider/Doc.kt
+++ src/main/kotlin/site/derker/kspider/Doc.kt
@@ -10,25 +10,29 @@ class Doc(html: String, private val spider: Spider) {
 
     fun queryByCss(selector: String, handler: Ele?.() -> Unit) {
         val jsoupEle = jsoupDoc.selectFirst(selector)
+        if (jsoupEle == null) {
+            handler(jsoupEle)
+        } else {
+            handler(Ele(jsoupEle, spider))
+        }
     }
 
-    fun queryAllByCss(selector: String, handler: List<Ele>?.() -> Unit) {
+    fun queryAllByCss(selector: String, handler: List<Ele>.(Spider) -> Unit) {
+        val jsoupEles = jsoupDoc.select(selector)
+        if (jsoupEles.isEmpty()) {
+            handler(listOf(), spider)
+        } else {
+            val eleList = jsoupEles.map { Ele(it, spider) }.toList()
+            handler(eleList, spider)
+        }
     }
 
     fun addUrls(vararg urls: String, parse: Doc.(Spider) -> Unit) {
         spider.addUrls(urls = urls, parse = parse)
     }
-
-    companion object {
-        fun parse(html: String): Doc {
-            val jDoc = Jsoup.parse(html)
-
-        }
-    }
-
 }
 
-class Ele(val ele: Element, private val spider: Spider) {
+class Ele(val jsoupEle: Element, private val spider: Spider) {
 
     fun addUrls(vararg urls: String, parse: Doc?.(Spider) -> Unit) {
         spider.addUrls(urls = urls, parse = parse)
